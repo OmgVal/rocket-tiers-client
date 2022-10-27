72990b0a487f56bb983e3209d1a6e977b81614c7
OmgValicious <ziallshamrock@gmail.com>
2022-10-27 11:08:54 -0700

    edit comment and profile added

--- src/components/pages/EditComment.jsx
+++ src/components/pages/EditComment.jsx
@@ -1,7 +1,68 @@
-export default function EditComment(){
-    return(
-        <>
-            EditComment
-        </>
+import { useEffect, useState } from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+
+export default function EditComment() {
+    const [form, setForm] = useState({
+        content: ''
+    })
+    const [msg, setMsg] = useState('')
+
+    const { id, commentid } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const getComment = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/tournaments/${id}/comments/${commentid}`)
+                setForm(response.data)
+            } catch (err) {
+                console.warn(err)
+                if (err.response) {
+                    setMsg(err.response.data.msg)
+                }
+            }
+        }
+        getComment()
+    }, [id, commentid])
+
+    const handleSubmit = async e => {
+        try {
+            e.preventDefault()
+            // axios.put/.post('url', data for the req body)
+            const response = await axios.put(`${process.env.REACT_APP_SERVER_URL}/api-v1/tournaments/${id}/comments/${commentid}`, form)
+            setForm(response.data)
+            navigate(`/posts/${id}`)
+        } catch (err) {
+            console.warn(err)
+            if (err.response) {
+                setMsg(err.response.data.msg)
+            }
+        }
+    }
+
+    return (
+        <div>
+            <h1>Edit Comment:</h1>
+            <p>{msg}</p>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='content'>Comment:</label>
+                    <input
+                        type='text'
+                        id='content'
+                        value={form.content}
+                        onChange={e => setForm({ ...form, content: e.target.value })}
+                    />
+                </div>
+
+                <button type='submit' style={{ backgroundColor: '#FC6767', width: '150px' }}>Submit</button>
+                <Link to={`/posts/${id}`}>
+                    <button style={{ backgroundColor: '#FC6767', width: '150px' }}>Cancel</button>
+                </Link>
+            </form>
+
+        </div>
     )
 }
--- src/components/pages/EditProfile.jsx
+++ src/components/pages/EditProfile.jsx
@@ -1,7 +1,116 @@
-export default function EditProfile(){
+import { useEffect, useState } from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+
+export default function EditProfile(handleLogout){
+    const { username } = useParams()
+    const [form, setForm] = useState({
+        username: username,
+        bio: ''
+        // profilePic: '' 
+        // edit user password?
+    })
+    const [msg, setMsg] = useState('')
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                // get the token from local storage
+                const token = localStorage.getItem('jwt')
+                // make the auth headers
+                const options = {
+                    headers: {
+                        'Authorization': token
+                    }
+                }
+                // hit the auth locked endpoint
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${username}`, options)
+                if (response.data.bio) {
+                    setForm({ ...form, bio: response.data.bio })
+                }
+                // console.log(response.data)
+            } catch (err) {
+                console.warn(err)
+                if (err.response) {
+                    setMsg(err.response.data.msg)
+                }
+            }
+        }
+        getUser()
+    }, [])
+
+    const handleSubmit = async e => {
+        try {
+            e.preventDefault()
+            // axios.put/.post('url', data for the req body)
+            const response = await axios.put(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${username}/edit`, form)
+            // navigate back to the details page for this bounty
+            setForm({ username: response.data.username, bio: response.data.bio })
+            navigate(`/${form.username}`)
+        } catch (err) {
+            console.warn(err)
+            if (err.response) {
+                setMsg(err.response.data.msg)
+            }
+        }
+    }
+    const handleDeleteUser = async (e) => {
+        try {
+            e.preventDefault()
+            // get the token from local storage
+            const token = localStorage.getItem('jwt')
+            // make the auth headers
+            const options = {
+                headers: {
+                    'Authorization': token
+                }
+            }
+            // hit the auth locked endpoint
+            const response = await axios.delete(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/${username}`, options)
+            handleLogout()
+            navigate('/')
+
+        } catch (err) {
+            console.warn(err)
+        }
+    }
+
     return(
         <>
-            EditProfile
+            <div>
+            <h1>Edit Profile:</h1>
+            <p>{msg}</p>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='username'>Username:</label>
+                    <input
+                        type='text'
+                        id='username'
+                        value={form.username}
+                        onChange={e => setForm({ ...form, username: e.target.value })}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='bio'>Bio:</label>
+                    <input
+                        type='text'
+                        id='bio'
+                        value={form.bio}
+                        onChange={e => setForm({ ...form, bio: e.target.value })}
+                    />
+                </div>
+
+                <button type='submit' style={{ backgroundColor: '#FC6767', width: '150px' }}>Submit</button>
+            </form>
+
+            <Link to={`/${username}`}>
+                <button style={{ backgroundColor: '#FC6767', width: '150px' }}>Cancel</button>
+            </Link>
+            <button onClick={handleDeleteUser} style={{ backgroundColor: 'red', width: '150px' }}>Delete Account</button>
+        </div>
         </>
     )
 }
